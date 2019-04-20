616c2774aca53142b2591623b0985c2c2e745caa
gelirdeniz <denizgelir@hamurlabs.com>
2019-04-21 00:55:13 +0300

    Initial Commit

---
+++ MLapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MLapp
+//
+//  Created by Deniz Gelir on 4/21/19.
+//  Copyright © 2019 Deniz Gelir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
